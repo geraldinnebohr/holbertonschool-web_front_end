789c71e879cd803a10c885d02b9fa0c3f4c488a0
geraldinnebohr <geraldinnebohr@gmail.com>
2020-06-22 19:56:12 -0500

    [FEATURE] files

---
+++ 0x0C-Javascript_advanced/5-mode.js
@@ -0,0 +1,10 @@
+function changeMode(size, weight, transform, background, color) {
+
+}
+
+function main() {
+  let spooky = changeMode(9, bold, uppercase, pink, green);
+  let darkMode = changeMode(12, bold, capitalize, black, white);
+  let screamMode = changeMode(12, normal, lowercase, white, black);
+  document.createElement()
+}
