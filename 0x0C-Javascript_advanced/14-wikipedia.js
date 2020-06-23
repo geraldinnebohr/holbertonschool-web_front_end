01772f29e20c6e15a11f8608609aac46dd9068e6
geraldinnebohr <geraldinnebohr@gmail.com>
2020-06-23 14:54:29 -0500

    [FIX] callbacks files

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -7,7 +7,17 @@ function createElement(data) {
 function queryWikipedia(callback) {
   let req = new XMLHttpRequest();
   req.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow");
-  req.responseText = () => callback()
+  req.onreadystatechange = function (event) {
+    if(req.readyState === 4) {
+      if(req.status === 200) {
+        callback(null, JSON.parse(req.responseText))
+      } else {
+        const error = new Error('Error');
+        return callback(error, null);
+      }
+    }
+  }
+  req.send();
 }
 
-queryWikipedia(createElement());
+queryWikipedia(createElement);
